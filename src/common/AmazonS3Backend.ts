ae85f377751aae12af6a5079d2aa580a1790e168
Alexandru Barbur <root.ctrlc@gmail.com>
2018-02-22 17:30:19 -0500

    Compute and display the Bucket base URL.
    This demonstrates how component bindings can influence the backend
    through the controller. It will also be used down the line for S3 API
    calls.

--- src/common/AmazonS3Backend.ts
+++ src/common/AmazonS3Backend.ts
@@ -3,14 +3,25 @@ import {IBackend} from './IBackend';
 
 export class AmazonS3Backend implements IBackend {
   /**
-   * Create an instance of the backend.
+   * Amazon S3 API endpoint.
    */
-  constructor(public region: string) { }
+  private baseUrl: string;
+
+  /**
+   * Create an instance of the backend.
+   * @see https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+   */
+  constructor(public region: string) {
+    this.baseUrl = `s3.${region}.amazonaws.com`;
+  }
 
   /**
    * Get a bucket with the given name.
    */
   getBucket(name: string): Bucket {
-    return new Bucket(name);
+    // when the bucket name contains a period ('.') we can't use https
+    // because it treats part of the bucket name as a subdomain and breaks SSL
+    let protocol = name.includes('.') ? 'http' : 'https';
+    return new Bucket(name, `${protocol}://${name}.${this.baseUrl}`);
   }
 }
--- src/common/Bucket.ts
+++ src/common/Bucket.ts
@@ -1,3 +1,3 @@
 export class Bucket {
-  constructor(public name: string) { }
+  constructor(public name: string, public baseUrl: string) { }
 }
