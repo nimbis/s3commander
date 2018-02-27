7423ce1998573dba656dc93f9fed0a36b8b8d87a
Alexandru Barbur <root.ctrlc@gmail.com>
2018-02-27 12:04:53 -0500

    Implement clone, equals, push, and pop methods for the Path class.
    This allows easier manipulation of Path objects.

--- src/common/Path.ts
+++ src/common/Path.ts
@@ -23,6 +23,20 @@ export class Path {
   }
 
   /**
+   * Create a deep copy of this path.
+   */
+  public clone(): Path {
+    return new Path(this.toString());
+  }
+
+  /**
+   * Check if this path is equal to the given path.
+   */
+  public equals(other: Path): boolean {
+    return (this.toString() === other.toString());
+  }
+
+  /**
    * Get path components.
    */
   public components(): string[] {
@@ -40,6 +54,36 @@ export class Path {
 
     // last component name
     return this.parts[this.parts.length - 1];
+  }
+
+  /**
+   * Push one or more components to the path.
+   */
+  public push(subpath: string): Path {
+    // split the subpath into components
+    let subparts = subpath.split('/').filter(function(part: string) {
+      return part.length > 0;
+    });
+
+    // update the existing components and folder flag
+    this.parts = this.parts.concat(subparts);
+    this.folder = (subpath.substring(subpath.length - 1) === '/');
+
+    // return the path so we can chain function calls
+    return this;
+  }
+
+  /**
+   * Pop a component from the path.
+   */
+  public pop(): Path {
+    // remove a trailing component if available
+    if (this.parts.length > 0) {
+      this.parts.pop();
+    }
+
+    // return the path so we can chain function calls
+    return this;
   }
 
   /**
