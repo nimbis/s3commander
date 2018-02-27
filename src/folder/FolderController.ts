45a8b36aed7e8c3f5c7ee6bc72ab169bcda0ffb9
Alexandru Barbur <root.ctrlc@gmail.com>
2018-02-27 12:08:43 -0500

    Add navigate and delete events to the folder controller and template.
    Prepare for exposing the methods as component bindings to the parent.

--- src/folder/FolderController.ts
+++ src/folder/FolderController.ts
@@ -5,4 +5,18 @@ export class FolderController {
    * Folder object. Passed in as component binding.
    */
   public folder: StorageObject;
+
+  /**
+   * Navigate to a folder. This is overriden by the component binding.
+   */
+  public onNavigate() {
+    // overriden by binding
+  }
+
+  /**
+   * Delete a folder. This is overriden by the component binding.
+   */
+  public onDelete() {
+    // overriden by binding
+  }
 }
