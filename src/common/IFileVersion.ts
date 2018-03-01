521b1c7554b4918baff2afad9fdc41aeca844e2b
Alexandru Barbur <root.ctrlc@gmail.com>
2018-03-01 01:04:49 -0500

    Document the IFileVersion interface.
    Document the interface and its' members.

--- src/common/IFileVersion.ts
+++ src/common/IFileVersion.ts
@@ -1,9 +1,24 @@
 /**
- * Represents an object that describes a specific version of a file.
+ * An object that represents a specific version of a file.
  */
 export interface IFileVersion {
+  /**
+   * Whether this version is currently the latest available one.
+   */
   latest: boolean;
+
+  /**
+   * Unique version identifier.
+   */
   versionId: string;
+
+  /**
+   * Last modified timestamp.
+   */
   lastModified: Date;
+
+  /**
+   * Whether this version represents a deletion marker.
+   */
   deleteMarker: boolean;
 }
