3316539f75be2251a95afc0a6c5b9d4df470f8ce
Alexandru Barbur <root.ctrlc@gmail.com>
2018-03-01 11:51:53 -0500

    Configure the s3commander module to whitelist form URLs.
    By default AngularJS doesn't allow you to interpolate form URLs unless
    they are whitelisted in the module's security policy. I added two URLs
    for the AWS S3 API to the whitelist.

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,17 @@ import {BucketComponent} from './bucket/BucketComponent';
 
 import './index.css';
 
+// register components and configure the module.
 angular
   .module('s3commander', [])
+  .config(function ($sceDelegateProvider: any) {
+    // https://docs.angularjs.org/api/ng/provider/$sceDelegateProvider
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'https://s3.amazonaws.com/*',
+      'https://*.s3.amazonaws.com/'
+    ]);
+  })
   .component('file', FileComponent)
   .component('folder', FolderComponent)
   .component('bucket', BucketComponent);
