d3b44548630afb99f9025a625fe07b0c23dd8a6e
Mike Liu <mike.liu.dev@gmail.com>
2019-04-05 13:07:41 -0400

    Fix webpack configuration for serving the build.
    This broke when creating the `build:dev` gulp target since both dev and
    serve both reference the same `webpack.conf.js` configuration file. This
    commit reverts the change so that files are compiled into the .tmp
    folder where the `serve` target expects them to be.

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -68,8 +68,8 @@ module.exports = {
   ],
   devtool: 'source-map',
   output: {
-    path: path.join(process.cwd(), conf.paths.dev),
-    filename: '[name]-s3commander.js'
+    path: path.join(process.cwd(), conf.paths.tmp),
+    filename: 'index.js'
   },
   resolve: {
     extensions: [
